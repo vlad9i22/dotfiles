5ccbe9272fc77565b2d5ae65dffe5b1e6fabdf06
Vlad <vlad9i2222@gmail.com>
2026-01-20 10:43:42 +0300

    Add templates

---
+++ Templates/shablonBool.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+bool solve() { 
+    using namespace std;
+}
+
+int main() {
+  std::ios::sync_with_stdio(0);
+  std::cin.tie(0);
+  int tasks = 1;
+  std::cin >> tasks;
+  while (tasks--) {
+    if (solve()) {
+      std::cout << "YES" << '\n';
+    } else {
+      std::cout << "NO" << '\n';
+    }
+  }
+}
